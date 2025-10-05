8c1d41e36fe71eb8ab4777ce5ebc889e7322bbcf
Franco Zanardi <francozanardi.97@gmail.com>
2025-10-04 22:02:53 -0300

    fix(font): if primary font is not available, fallback fonts are attempted to be used first (before falling back to default font)

--- src/pictex/text/font_manager.py
+++ src/pictex/text/font_manager.py
@@ -13,8 +13,8 @@ class FontManager:
     def __init__(self, style: Style, font_smoothing: FontSmoothing):
         self._style = style
         self._font_smoothing = font_smoothing
-        self._primary_font = self._create_font(self._style.font_family.get())
         self._fallback_font_typefaces = self._prepare_fallbacks()
+        self._primary_font = self._create_primary_font()
 
     def get_primary_font(self) -> skia.Font:
         return self._primary_font
@@ -22,10 +22,22 @@ class FontManager:
     def get_fallback_font_typefaces(self) -> List[skia.Typeface]:
         return self._fallback_font_typefaces
     
-    def _create_font(self, font_path_or_name: Optional[str]) -> skia.Font:
-        typeface = self._create_font_typeface(font_path_or_name)
+    def _create_primary_font(self) -> skia.Font:
+        font_path_or_name = self._style.font_family.get()
+        typeface: Optional[skia.Typeface] = None
+        if font_path_or_name:
+            typeface = self._create_font_typeface(font_path_or_name)
+        
+        if len(self._fallback_font_typefaces) > 0:
+            # the first fallback font typeface is promoted to primary font
+            typeface = self._fallback_font_typefaces.pop(0)
+
         if not typeface:
             typeface = TypefaceLoader.load_default()
+        
+        return self._create_font(typeface)
+    
+    def _create_font(self, typeface: skia.Typeface) -> skia.Font:
         font = skia.Font(typeface, self._style.font_size.get())
         if self._font_smoothing == FontSmoothing.SUBPIXEL:
             font.setEdging(skia.Font.Edging.kSubpixelAntiAlias)
@@ -35,10 +47,7 @@ class FontManager:
             font.setSubpixel(False)
         return font
 
-    def _create_font_typeface(self, font_path_or_name: Optional[str]) -> skia.Typeface:
-        if font_path_or_name is None:
-            return TypefaceLoader.load_default()
-
+    def _create_font_typeface(self, font_path_or_name: str) -> Optional[skia.Typeface]:
         if not os.path.exists(font_path_or_name):
             return self._create_system_font_typeface(font_path_or_name)
         
@@ -54,7 +63,7 @@ class FontManager:
         
         return typeface
     
-    def _create_system_font_typeface(self, font_family: str) -> Optional[skia.Font]:
+    def _create_system_font_typeface(self, font_family: str) -> Optional[skia.Typeface]:
         font_style = skia.FontStyle(
             weight=self._style.font_weight.get(),
             width=skia.FontStyle.kNormal_Width,
